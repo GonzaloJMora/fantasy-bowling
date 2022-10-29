f9c7417df745c2861c6081c6ade7795d48c6d04a
Rithik Theddu <48131404+RithikTheddu1999@users.noreply.github.com>
2022-10-29 15:58:32 -0400

    Add files via upload
    Scoring logic

---
+++ Bowling_logic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bowling_logic : MonoBehaviour
+{
+    public Transform pin;
+    public float max = .6f; // can be changed with UI
+    public int points = 1; 
+    public Score score; //score object from score.cs
+
+
+    void Awake()
+    {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<Score>();
+    }
+    void FellOrNot()
+    {
+        try
+        {
+            if(pin.up.y < max)
+            {
+                score.Add(points);
+                gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+        catch
+        {
+            Debug.log("Pins are not working properly, it might be in a dead zone");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        FellOrNot();
+    }
+   
+}
